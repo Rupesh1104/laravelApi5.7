b02f2dafc32c3f88275c85b6280e5d2857d71a46
Rupesh1104 <Rupesh1104>
2020-04-26 00:03:01 +0530

    passport installed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -23,7 +23,7 @@ class ProductController extends Controller
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         //

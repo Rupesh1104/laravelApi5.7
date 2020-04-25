4a297d4ed8d6def333039c18a6f473d6eaaa1d39
Rupesh1104 <Rupesh1104>
2020-04-26 00:35:44 +0530

    Product created using oauth

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class ProductController extends Controller
 {
@@ -14,6 +17,11 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware("auth:api")->except('index','show');
+    }
+
     public function index()
     {
         return ProductCollection::collection(Product::paginate(20));
@@ -35,9 +43,18 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $obj= new Product();
+        $obj->name=$request->name;
+        $obj->detail=$request->description;
+        $obj->stock=$request->stock;
+        $obj->price=$request->price;
+        $obj->discount=$request->discount;
+        $obj->save();
+        return response([
+           'data' => new ProductResource($obj)
+        ],Response::HTTP_CREATED);
     }
 
     /**
